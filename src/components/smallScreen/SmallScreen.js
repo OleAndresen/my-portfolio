637ff559d8ec389c70a82f82300c7285beabe6b7
Ole <116582322+OleAndresen@users.noreply.github.com>
2022-11-16 12:33:21 +0100

    Quick Update
    Html Content + Social Media Links

--- src/components/smallScreen/SmallScreen.js
+++ src/components/smallScreen/SmallScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter} from 'react-icons/ai'
 
 function SmallScreen() {
   return (
@@ -9,6 +10,11 @@ function SmallScreen() {
               <span className='text-green-500'>Ole</span>
             </h1>
             <h1 className='mt-3 tracking-tight font-extrabold text-white'>Frontend Developer</h1>
+            <div className='mt-3 flex justify-center'>
+              <a className='text-blue-500' href="https://www.linkedin.com/in/ole-andresen-in" target={'_blank'} rel="noreferrer"><AiFillLinkedin className='text-2xl' /></a>
+              <a className='text-white ml-4 mr-4' href="https://github.com/OleAndresen" target={'_blank'} rel="noreferrer"><AiFillGithub className='text-2xl' /></a>
+              <a className='text-sky-600' href="https://twitter.com/OleAndresen_" target={'_blank'} rel="noreferrer"><AiOutlineTwitter className='text-2xl' /></a>
+            </div>
         </section>
     </div>
   )
